79d4abd83f80d3ae70c759edf0f9e9aa50c4fef5
Stephen Akuffo <sa.akuffo@gmail.com>
2021-07-05 01:50:25 -0400

    repo start

---
+++ git/install_git.sh
@@ -0,0 +1,7 @@
+# update the system to the latest version of git
+sudo apt update
+sudo apt install git
+
+# configure git to with a user name and email address
+git config --global user.name "Stephen Akuffo"
+git config --global user.email "sa.akuffo@gmail.com"
